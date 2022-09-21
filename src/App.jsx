ddff77f33c0ff2403b343e6a6a5bf0e6146e472f
Cristhian Guerrero <reguerre@espol.edu.ec>
2022-09-21 05:00:41 -0500

    Version inicial del proyecto | redux toolkit counter

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import reactLogo from './assets/react.svg';
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment } from './store/slices/counter/counterSlice';
+
+function App() {
+
+  const {value: count} = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => dispatch(increment())}>
+          count is {count}
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
